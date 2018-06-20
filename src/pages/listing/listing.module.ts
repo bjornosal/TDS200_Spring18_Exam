38fac5b0a18266114aeead6c1fef58431b48a652
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-06-20 20:54:53 +0200

    Changed README to proper README.

--- src/pages/listing/listing.module.ts
+++ src/pages/listing/listing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { ListingPage } from './listing';
-import { MessagePage } from '../message/message';
 import { ComponentsModule } from '../../components/components.module';
 
 @NgModule({
